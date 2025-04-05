f25cfb4cef2e2c1612209ca61ee258f71ad223e7
C:\Users\Marina e Alice\Documents\Meus Jogos\Waves of a Distant Place\Godot Project Folder <marinadufrance@gmail.com>
2025-04-05 05:22:51 -0300

    Player_Standard_RunCycle Test1 CRT Shader

---
+++ Entities/Player/Player_Movement.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+var max_speed :int = 100
+var acceleration:int = 75
+var input_vector:=Vector2.ZERO
+@export var animation_player:AnimationPlayer
+
+var player_can_move:=true
+
+func _physics_process(delta: float) -> void:
+	movement()
+		
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_pressed("movement_action"):
+		if player_can_move:
+			input_vector.x = Input.get_axis("ui_left","ui_right")
+	if Input.is_action_just_released("movement_action"):
+			input_vector = Vector2.ZERO	
+			
+func movement():
+	velocity = velocity.move_toward(max_speed*input_vector,acceleration)
+	if input_vector.x>0:
+		animation_player.play("Run_Standard_E")
+	elif input_vector.x<0:
+		animation_player.play("Run_Standard_W")
+	else:
+		animation_player.stop()
+	move_and_slide()
